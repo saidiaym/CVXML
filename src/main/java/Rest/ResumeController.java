53241d64f3b41a04e2a83f57d5788caa3d212ba6
saidiaym <saidiaym@gmail.com>
2014-05-07 15:43:23 +0200

    finition de serveur  ajout de la methode put serveur en bon etat

--- src/main/java/Rest/ResumeController.java
+++ src/main/java/Rest/ResumeController.java
@@ -2,10 +2,7 @@ package Rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by saidiaym on 08/04/14.
@@ -57,7 +54,7 @@ public class ResumeController {
         resumeList.CVS.add(resume1);
 
     }
-    @RequestMapping(value="/all", method = RequestMethod.GET )
+    @RequestMapping(value="", method = RequestMethod.GET )
     public @ResponseBody
     ResumeList getListInXML() {
 
@@ -84,39 +81,18 @@ public class ResumeController {
         return  null;
 
 
+    }
+    @RequestMapping(method = RequestMethod.PUT)
+    @ResponseBody
+    public String putcv(@RequestBody Resume resume){
 
-
+      resumeList.CVS.add(resume);
+        return "CV ajouter au serveur";
     }
 
-    @RequestMapping(value="{identifiant}/{nom}/{prenom}/{mail}/adresse/{ad}/competences/{c1}/{nc1}/{c2}/{nc2}" +
-            "/experiences/{e1}/{ne1}/{e2}/{ne2}/langues/{l1}/{nl1}/{l2}/{nl2}" +
-            "/loisirs/{lo1}", method = RequestMethod.GET)
-    public @ResponseBody
-    ResumeList  putCVInXML(@PathVariable String identifiant, @PathVariable String nom, @PathVariable String prenom, @PathVariable String mail,@PathVariable String ad,
-                          @PathVariable String c1, @PathVariable String nc1, @PathVariable String c2, @PathVariable String nc2,
-                          @PathVariable String e1, @PathVariable String ne1, @PathVariable String e2, @PathVariable String ne2,
-                          @PathVariable String l1, @PathVariable String nl1, @PathVariable String l2, @PathVariable String nl2,
-                          @PathVariable String lo1) {
-
-        ResumeCompetences Rcomp = new ResumeCompetences();
-        ResumeExperiences Rexp = new ResumeExperiences();
-        ResumeLangue Rlangue = new ResumeLangue();
-        ResumeLoisirs Rloisirs = new ResumeLoisirs();
-        ResumeAdresse Radresse = new ResumeAdresse();
-        Radresse.addResumeAdresse(ad);
-        Rcomp.addCompetences(c1,nc1);
-        Rcomp.addCompetences(c2,nc2);
-        Rexp.addExperience(e1, ne1);
-        Rexp.addExperience(e2,ne2);
-        Rlangue.addLangue(l1, nl1);
-        Rlangue.addLangue(l2,nl2);
-        Rloisirs.addLoisir(lo1);
-
-        Resume cv= new Resume(identifiant,nom,prenom,mail,Radresse, Rlangue,Rloisirs,Rexp,Rcomp);
-        resumeList.CVS.add(cv);
-        return resumeList;
 
 
-    }
+
+
 
 }
